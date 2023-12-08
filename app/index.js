f022dbeebbde03f9a569af852cc8e5efaf76c9cb
David Ervin <dnervin@student.fullsail.edu>
2023-12-08 11:24:17 -0500

    Add Express app and root endpoint

---
+++ app/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'API is running!',
+    success: true,
+  });
+});
+
+module.exports = app;
