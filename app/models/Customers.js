1b1c61182e02f03609bb120d148873859e35b80a
David Ervin <dnervin@student.fullsail.edu>
2023-12-08 17:00:38 -0500

    Add validation to schema

--- app/models/Customers.js
+++ app/models/Customers.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose');
 
-const customerSchema = new mongoose.Schema({
-  name: {
-    type: String,
+const customerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      trim: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Please provide a valid email address',
+      ],
+    },
   },
-  email: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Customer', customerSchema);
