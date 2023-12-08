23dd5ddaed3f53fbc6ddac6cc58f958736a25a35
David Ervin <dnervin@student.fullsail.edu>
2023-12-08 15:05:27 -0500

    Add Customers model

---
+++ app/models/Customers.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const customerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model('Customer', customerSchema);
