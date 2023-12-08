16e8086e5306cb22e82bf72361a14c92df3289d8
David Ervin <dnervin@student.fullsail.edu>
2023-12-08 13:56:21 -0500

    Refactor customerRoutes to use controller functions

--- app/routes/customerRoutes.js
+++ app/routes/customerRoutes.js
@@ -1,52 +1,26 @@
+const {
+  getAllCustomers,
+  getCustomerById,
+  updateCustomer,
+  createCustomer,
+  deleteCustomer,
+} = require('../controllers/customerContoller');
+
 const router = require('express').Router();
 
-// GET all customers from /api/v1/customer
-router.get('/', (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: `${req.method} - Request all customers!`,
-  });
-});
+// GET from /api/v1/customer
+router.get('/', getAllCustomers);
 
-// GET a customer from /api/v1/customer/:id
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
+// GET from /api/v1/customer/:id
+router.get('/:id', getCustomerById);
 
-  res.status(200).json({
-    id,
-    success: true,
-    message: `${req.method} - Request a customer!`,
-  });
-});
+// POST to /api/v1/customer
+router.post('/', createCustomer);
 
-// POST a customer to /api/v1/customer
-router.post('/', (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: `${req.method} - Create a customer!`,
-  });
-});
+// PUT to /api/v1/customer/:id
+router.put('/:id', updateCustomer);
 
-// PUT a customer to /api/v1/customer/:id
-router.put('/:id', (req, res) => {
-  const { id } = req.params;
-
-  res.status(200).json({
-    id,
-    success: true,
-    message: `${req.method} - Update a customer!`,
-  });
-});
-
-// DELETE a customer from /api/v1/customer/:id
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-
-  res.status(200).json({
-    id,
-    message: `${req.method} - Delete a customer!`,
-    success: true,
-  });
-});
+// DELETE from /api/v1/customer/:id
+router.delete('/:id', deleteCustomer);
 
 module.exports = router;
