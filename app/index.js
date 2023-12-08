e12f74445690b26f89f2d0a0d5a229a60323e264
David Ervin <dnervin@student.fullsail.edu>
2023-12-08 15:10:46 -0500

    Add morgan middleware for logging requests

--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
 // app/index.js
 
 const express = require('express');
+const morgan = require('morgan');
+
 const routeHandler = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.status(200).json({
