6603f1d3ea0f223f881ae31e4426d19a28c1d8a6
David Ervin <dnervin@student.fullsail.edu>
2023-12-08 13:02:29 -0500

    Add /api/v1 route to app

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
+// app/index.js
+
 const express = require('express');
+const routeHandler = require('./routes');
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).json({
@@ -8,5 +13,7 @@ app.get('/', (req, res) => {
     success: true,
   });
 });
+
+app.use('/api/v1', routeHandler);
 
 module.exports = app;
