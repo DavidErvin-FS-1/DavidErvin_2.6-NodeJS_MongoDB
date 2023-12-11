a8f4fe4095402c6ab61b2996a62e21a95f0a3e5b
David Ervin <dnervin@student.fullsail.edu>
2023-12-11 15:11:52 -0500

    Refactor order schema

--- app/models/Orders.js
+++ app/models/Orders.js
@@ -1,22 +1,40 @@
 const mongoose = require('mongoose');
 const moment = require('moment');
 
-const orderSchema = new mongoose.Schema(
-  {
-    date: {
-      type: String,
-      default: moment(Date.now).format('YYYY-MM-DD'),
-    },
-    amount: {
-      type: Number,
-      default: 0,
-    },
-    customer: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Customer',
-    },
+const orderSchema = new mongoose.Schema({
+  customer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Customer',
+    required: true,
   },
-  { timestamps: true }
-);
+  products: [
+    {
+      name: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        min: 1,
+      },
+    },
+  ],
+  orderDate: {
+    type: Date,
+    default: Date.now,
+  },
+  totalAmount: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  status: {
+    type: String,
+    enum: ['Pending', 'Shipped', 'Delivered'],
+    default: 'Pending',
+  },
+});
 
 module.exports = mongoose.model('Order', orderSchema);
