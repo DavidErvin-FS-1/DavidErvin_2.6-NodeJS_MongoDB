7f7eb69bba405dfff1a1a46b6dc0179ae2406e01
David Ervin <dnervin@student.fullsail.edu>
2023-12-15 14:15:44 -0500

    Refactor orderController.js and add messages.js

--- app/controllers/orderContoller.js
+++ app/controllers/orderContoller.js
@@ -1,86 +1,109 @@
-const Order = require('../models/Orders');
-const { errorHandler } = require('../helpers/errorHandler');
+const Order = require('../models/Orders')
+const { errorHandler } = require('../helpers/errorHandler')
+const Messages = require('../helpers/messages')
 
 const getAllOrders = async (req, res) => {
   try {
-    const orders = await Order.find({});
+    const orders = await Order.find({}).select('-__v').populate('customer')
 
     res.status(200).json({
       data: orders,
       success: true,
-      message: `${req.method} - Request all orders!`,
-    });
+      message: Messages.getAllOrders(req.method),
+    })
   } catch (error) {
-    errorHandler(res, 500, 'Server error', error);
+    errorHandler(res, 500, Messages.serverError, error)
   }
-};
+}
 
 const getOrderById = async (req, res) => {
   try {
-    const { id } = req.params;
-    const order = await Order.findById(id);
-    console.log(order);
+    const { id } = req.params
+    const order = await Order.findById(id).select('-__v').populate('customer')
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: Messages.notFound,
+      })
+    }
 
     res.status(200).json({
       data: order,
       success: true,
-      message: `${req.method} - Request a order!`,
-    });
+      message: Messages.getOrderById(req.method),
+    })
   } catch (error) {
-    errorHandler(res, 500, 'Server error', error);
+    errorHandler(res, 500, Messages.serverError, error)
   }
-};
+}
 
 const createOrder = async (req, res) => {
   try {
-    const data = req.body;
-    const order = await Order.create(data);
-    console.log(order);
+    const data = req.body
+    const order = await Order.create(data)
+    console.log(order)
 
     res.status(200).json({
       success: true,
-      message: `${req.method} - Create a order!`,
-    });
+      message: Messages.createOrder(req.method),
+    })
   } catch (error) {
-    errorHandler(res, 500, 'Server error', error);
+    errorHandler(res, 500, Messages.serverError, error)
   }
-};
+}
 
 const updateOrder = async (req, res) => {
   try {
-    const { id } = req.params;
-    const data = req.body;
+    const { id } = req.params
+    const data = req.body
 
     const order = await Order.findByIdAndUpdate(id, data, {
       new: true,
-    });
-    console.log(order);
+    }).select('-__v')
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: Messages.notFound,
+      })
+    }
+
+    console.log(order)
 
     res.status(200).json({
       data: order,
       success: true,
-      message: `${req.method} - Update a order!`,
-    });
+      message: Messages.updateOrder(req.method),
+    })
   } catch (error) {
-    errorHandler(res, 500, 'Server error', error);
+    errorHandler(res, 500, Messages.serverError, error)
   }
-};
+}
 
 const deleteOrder = async (req, res) => {
   try {
-    const { id } = req.params;
-    const order = await Order.findByIdAndDelete(id);
-    console.log(order);
+    const { id } = req.params
+    const order = await Order.findByIdAndDelete(id).select('-__v')
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: Messages.notFound,
+      })
+    }
+
+    console.log(order)
 
     res.status(200).json({
       id,
-      message: `${req.method} - Delete a order!`,
+      message: Messages.deleteOrder(req.method),
       success: true,
-    });
+    })
   } catch (error) {
-    errorHandler(res, 500, 'Server error', error);
+    errorHandler(res, 500, Messages.serverError, error)
   }
-};
+}
 
 module.exports = {
   getAllOrders,
@@ -88,4 +111,4 @@ module.exports = {
   createOrder,
   updateOrder,
   deleteOrder,
-};
+}
---
+++ app/helpers/messages.js
@@ -0,0 +1,9 @@
+module.exports = {
+  notFound: 'Object not found',
+  serverError: 'Server error',
+  getAllOrders: (method) => `${method} - Request all orders!`,
+  getOrderById: (method) => `${method} - Request an order!`,
+  createOrder: (method) => `${method} - Create an order!`,
+  updateOrder: (method) => `${method} - Update an order!`,
+  deleteOrder: (method) => `${method} - Delete an order!`,
+}
